986ca82255639b62062149f5bf1f22753278a357
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2023-03-30 23:06:04 -0700

    347. Find the top k frequent numbers (array based implementation).

--- ruby/array_hashing/347_top_k_frequent_elements.rb
+++ ruby/array_hashing/347_top_k_frequent_elements.rb
@@ -2,31 +2,33 @@
 # @param {Integer} k
 # @return {Integer[]}
 def top_k_frequent(nums, k)
-  count = {}
+  num_count = {}
   nums.each do |num|
-    count[num] += 1 if count.key?(num)
-    count[num] = 1 unless count.key?(num)
+    num_count[num] += 1 if num_count.key?(num)
+    num_count[num] = 1 unless num_count.key?(num)
   end
 
-  sorted_count = count.sort_by { |_, c| -c }.to_h
+  frequency = Array.new(nums.size + 1, [])
+  num_count.each { |num, count| frequency[count] += [num] }
 
-  sorted_count.keys.take(k)
+  top = []
+  frequency.reverse_each do |f|
+    break if k <= 0
+
+    k -= f.size and top += f unless f.empty?
+  end
+
+  top
 end
 
-# space: O(n) since we use a hash
-# complexity:
-#   O(n log n) sorting the hash
-
 def test
-  # Test cases
-
-  # result: [1,2]
+  # result: [1, 2]
   puts top_k_frequent([1, 1, 1, 2, 2, 3], 2).inspect
 
   # result: [1]
   puts top_k_frequent([1], 1).inspect
 
-  # result: [5,2,1]
+  # result: [5, 2, 1]
   puts top_k_frequent([10, 1, 1, 1, 1, 1, 5, 5, 5, 5, 2, 2, 2, 1, 0], 3).inspect
 end
 
