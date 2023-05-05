d6c94f94760d3f1c2b5a4db14854c6cdf929551d
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2023-05-04 19:04:51 -0700

    49. group anagrams. (ruby improvement)

--- ruby/array_hashing/49_group_anagrams.rb
+++ ruby/array_hashing/49_group_anagrams.rb
@@ -1,52 +1,30 @@
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
-  return [] if strs.empty?
+  return [strs] if strs.size <= 1
 
   result = Hash.new([])
+  count = Array.new(26, 0)
 
-  until strs.empty?
-    strs_copy = strs.clone
-    current = strs.shift
-    strs_copy.shift
-
-    current_count = 0
-    # this solution won't work for all strings, since two string not anagrams can have the same count
-    # what i can do is have a multi index array at each count, and look for the internal array where the anagram is valid to push
-    current.each_char { |char| current_count += char.ord - "a".ord }
-    puts current_count
-    result[current_count] = [].push(current)
-    strs_copy.each do |str|
-      index = strs.find_index(str)
-      if anagram?(current, str)
-        p str, current_count
-        result[current_count].push(str) and strs.delete_at(index)
-        p result
-      end
+  strs.each do |str|
+    str.each_char { |char| count[char.ord - "a".ord] += 1 }
+    if result.key?(count)
+      result[count].push(str)
+    else
+      result[count] = [str]
     end
+    count = Array.new(26, 0)
   end
-  p result
+
   result.values
 end
 
-def anagram?(str1, str2)
-  char_count = Hash.new(0)
-  str1.each_char { |char| char_count[char] += 1 }
-
-  str2.each_char do |char|
-    return false unless char_count.key? char
-
-    char_count[char] -= 1
-  end
-  char_count.values.sum.zero?
-end
-
 def test
-  #puts group_anagrams(%w[eat tea tan ate nat bat]).inspect
+  puts group_anagrams(%w[eat tea tan ate nat bat]).inspect
 
-  #puts group_anagrams([""]).inspect
+  puts group_anagrams([""]).inspect
 
-  #puts group_anagrams(["a"]).inspect
+  puts group_anagrams(["a"]).inspect
 
   puts group_anagrams(%w[a a aa ab]).inspect
 end
